f1525d481d984b7a8846f958c24370100da801b1
Vamshi3005 <122156996+Vamshi3005@users.noreply.github.com>
2024-12-10 23:01:26 +0530

    Update AuthFunction.cs

--- AuthFunction.cs
+++ AuthFunction.cs
@@ -1,54 +1,29 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Identity.Client;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace AzureFunctionApp
 {
-    public class AuthFunction
+    public static class AuthFunction
     {
-        private readonly ILogger<AuthFunction> _logger;
-
-        public AuthFunction(ILogger<AuthFunction> logger)
-        {
-            _logger = logger;
-        }
+        private static string clientId = "ce295dbb-20b4-4dfe-9aae-a5c93cae6ba0";
+        private static string tenantId = "418ddeda-4cb4-493f-881b-69cd97e34fa5";
+        private static string redirectUri = "https://testing001vk.azurewebsites.net/api/AuthFunction";
 
         [Function("AuthFunction")]
-        public async Task<HttpResponseData> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public static async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
         {
-            _logger.LogInformation("AuthFunction invoked.");
+            var logger = executionContext.GetLogger("AuthFunction");
+            logger.LogInformation("Processing a request.");
 
-            string clientId = "ce295dbb-20b4-4dfe-9aae-a5c93cae6ba0";
-            string tenantId = "418ddeda-4cb4-493f-881b-69cd97e34fa5";
-            string redirectUri = "https://testing001vk.azurewebsites.net/api/AuthFunction";
+            var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            await response.WriteStringAsync("Hello from Azure Function!");
 
-            var pca = PublicClientApplicationBuilder
-                .Create(clientId)
-                .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
-                .WithRedirectUri(redirectUri)
-                .Build();
-
-            try
-            {
-                var scopes = new[] { "User.Read" };
-                var result = await pca.AcquireTokenInteractive(scopes).ExecuteAsync();
-
-                var response = req.CreateResponse(HttpStatusCode.OK);
-                await response.WriteStringAsync($"Access Token: {result.AccessToken}");
-                return response;
-            }
-            catch (MsalException ex)
-            {
-                _logger.LogError($"Authentication error: {ex.Message}");
-
-                var response = req.CreateResponse(HttpStatusCode.BadRequest);
-                await response.WriteStringAsync($"Authentication error: {ex.Message}");
-                return response;
-            }
+            return response;
         }
     }
 }
